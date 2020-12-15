7b73446923183841cb4b5f72d77dabe7f0b20116
Dave Redmin <david.redmin@trio.dhs.gov>
2020-12-15 16:50:56 -0500

    Define PCA public and private subnets

---
+++ subnets.tf
@@ -0,0 +1,24 @@
+#-------------------------------------------------------------------------------
+# Create the subnets for the shared services VPC.
+#-------------------------------------------------------------------------------
+module "private" {
+  source = "github.com/cisagov/distributed-subnets-tf-module"
+  providers = {
+    aws = aws.pca_provisionaccount
+  }
+
+  vpc_id             = aws_vpc.pca.id
+  subnet_cidr_blocks = var.private_subnet_cidr_blocks
+  tags               = var.tags
+}
+
+module "public" {
+  source = "github.com/cisagov/distributed-subnets-tf-module"
+  providers = {
+    aws = aws.pca_provisionaccount
+  }
+
+  vpc_id             = aws_vpc.pca.id
+  subnet_cidr_blocks = var.public_subnet_cidr_blocks
+  tags               = var.tags
+}
